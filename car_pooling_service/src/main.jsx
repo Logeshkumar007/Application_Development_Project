2475dd0d938cd9fe2e0374de9a003fef95b705c5
Nithishuchiha <124854933+Nithishuchiha@users.noreply.github.com>
2024-08-05 21:56:50 +0530

    updated

--- car_pooling_service/src/main.jsx
+++ car_pooling_service/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./oldComponents/App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./components/Store/store.jsx";
